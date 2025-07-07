1aa00cdf9adfe4d517c7583eff7ef5a9a6ab32f8
Safet <savrgovcevic@edu.aau.at>
2025-07-07 14:38:44 +0200

    Initial commit with tourbackend and tourfrontend

---
+++ tourbackend/src/main/kotlin/com/group8/guidetour/tourbackend/TourbackendApplication.kt
@@ -0,0 +1,11 @@
+package com.group8.guidetour.tourbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TourbackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TourbackendApplication>(*args)
+}
---
+++ tourbackend/src/test/kotlin/com/group8/guidetour/tourbackend/TourbackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.group8.guidetour.tourbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TourbackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
